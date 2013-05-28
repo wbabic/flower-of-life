a3dfd214f466b543c8465e26574f4cad1f26bdeb
Walter P Babic <wbabic@mac.com>
2013-05-28 16:52:34 -0600

    solution reached; cleanup needed

--- src/edn_play/power-set.clj
+++ src/edn_play/power-set.clj
@@ -1,6 +1,7 @@
 (ns edn-play.power-set)
 
 (comment
+  "use reductions to see the process"
   (defn power-set [s]
   (reductions (fn [ss x]
                 (concat ss (map #(conj % x) ss)))
@@ -36,7 +37,6 @@
   ;;=> ([1] [] [1 2] [2])
   )
 
-
 (comment
   (map (partial reduce +) (power-set #{1 2 3}))
   )
@@ -58,4 +58,146 @@
 
 (comment
   (sum-nonempty-set #{1 2 3})
+  ;;=> (1 2 3 3 4 5 6)
+  )
+
+(defn some-subset-equal-sum [sets]
+  (not (empty? (apply clojure.set/intersection
+                      (map (comp set sum-nonempty-set) sets)))))
+
+(comment
+  (def sample-set-1
+    (list
+     #{-1 3 -5 7 -9 11 -13 15}
+     #{1 -3 5 -7 9 -11 13 -15}
+     #{1 -1 2 -2 4 -4 8 -8}))
+  (def sample-set-2
+    (list
+     #{-1 -2 -3 -4 -5 -6}
+     #{1 2 3 4 5 6 7 8 9}))
+  (def sample-set-3
+    (list
+     #{1 2} #{10 20}))
+  
+  ((comp set sum-nonempty-set) (first sample-set-1))
+  
+  (some-subset-equal-sum sample-set-1)
+  (some-subset-equal-sum sample-set-2)
+
+  )
+
+(defn power-set-2 [sets]
+  (map
+   (fn [set] (filter (complement empty?)
+                    (reduce
+                     (fn [ss x]
+                       (concat ss
+                               (map
+                                (fn [s] (conj s x))
+                                ss)))
+                     [[]]
+                     set)))
+   sets))
+
+(comment
+  (power-set-2 sample-set-3)
+  ;;=> (([1] [2] [1 2]) ([10] [20] [10 20]))
+
+  (def f '([1] [2] [1 2]))
+  ;; sum each vector; change into #{1 2 3}
+
+  (into #{} (map (partial reduce +) f))
+  ;;=> #{1 2 3}
+
+  (map
+   (fn [p]
+     (into #{} (map (partial reduce +) p)))
+   (power-set-2 sample-set-3))
+  ;;=> (#{1 2 3} #{10 20 30})
+
+  (apply clojure.set/intersection
+         (map
+          (fn [p]
+            (into #{} (map (partial reduce +) p)))
+          (power-set-2 sample-set-3)))
+  ;;=> #{}
+
+  (apply clojure.set/intersection
+         (map
+          (fn [p]
+            (into #{} (map (partial reduce +) p)))
+          (power-set-2 sample-set-1)))
+  ;;=> #{-1 0 1 -2 -3 2 3 -4 -5 4 -6 5 -7 6 7 -8 -9 8 -10 9 -11 10 -12 11 -13 12 -14 13 -15 14 15}
+
+  ((complement empty?)
+   (apply clojure.set/intersection
+          (map
+           (fn [p]
+             (into #{} (map (partial reduce +) p)))
+           (power-set-2 sample-set-1))))
+  ;;=> true
+
+  ((complement empty?)
+   (apply clojure.set/intersection
+          (map
+           (fn [p]
+             (into #{} (map (partial reduce +) p)))
+           (power-set-2 sample-set-3))))
+  
+  ;;=> false
+  (defn s1 [ps]
+    ((complement empty?)
+     (apply clojure.set/intersection
+            (map
+             (fn [p]
+               (into #{} (map (partial reduce +) p)))
+             ps))))
+
+  (s1 (power-set-2 sample-set-3))
+  (s1 (power-set-2 sample-set-1))
+
+  ((fn [& sets]
+     (let [power-set
+           (fn [sets]
+             (map
+              (fn [set] (filter (complement empty?)
+                               (reduce
+                                (fn [ss x]
+                                  (concat ss
+                                          (map
+                                           (fn [s] (conj s x))
+                                           ss)))
+                                [[]]
+                                set)))
+              sets))]
+       ((complement empty?)
+        (apply clojure.set/intersection
+               (map
+                (fn [p]
+                  (into #{} (map (partial reduce +) p)))
+                (power-set sets))))))
+   #{-1 1 99} 
+   #{-2 2 888}
+   #{-3 3 7777})
+
+  (fn [& sets]
+     (let [power-set
+           (fn [sets]
+             (map
+              (fn [set] (filter (complement empty?)
+                               (reduce
+                                (fn [ss x]
+                                  (concat ss
+                                          (map
+                                           (fn [s] (conj s x))
+                                           ss)))
+                                [[]]
+                                set)))
+              sets))]
+       ((complement empty?)
+        (apply clojure.set/intersection
+               (map
+                (fn [p]
+                  (into #{} (map (partial reduce +) p)))
+                (power-set sets))))))
   )
